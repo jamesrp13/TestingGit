4971b89c44f139413e356e18af374d070d513ad6
Kaleo <kauakim@gmail.com>
2016-03-23 09:32:24 -0600

    deleted view did load

--- TestingGit/ViewController.swift
+++ TestingGit/ViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // I am adding something for .git testing in viewDidLoad
-        
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
