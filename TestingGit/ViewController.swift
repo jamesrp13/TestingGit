1e192f383a2c7ebe043da3672f2f777dee1354cc
James <jamesrp13@gmail.com>
2016-03-23 09:41:40 -0600

    Revert "Added comment to viewDidLoad"
    This reverts commit a8e60cda551bca54c65100d4bb40d355a372ed1f.
    
    # Conflicts:
    #	TestingGit.xcodeproj/project.xcworkspace/xcuserdata/James.xcuserdatad/UserInterfaceState.xcuserstate
    #	TestingGit/ViewController.swift

--- TestingGit/ViewController.swift
+++ TestingGit/ViewController.swift
@@ -13,8 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // I am adding something for .git testing in viewDidLoad
-        //Adding another comment
     }
 
     override func didReceiveMemoryWarning() {
